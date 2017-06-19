21900e87bc0d6fa8ea8706c5d04d39a02d0b4df1
timwis <tim@timwis.com>
2017-06-19 08:24:59 -0400

    add schema, use db-friendly column names

--- main.py
+++ main.py
@@ -23,8 +23,20 @@ def add_race_ethnicity(row):
 def process(hires):
     # Exclude promotions, replace race fields with one
     table = etl.fromcsv(hires) \
-        .select('{Latest Start Date} == {Job Seniority Date}') \
-        .addfield('Race/Ethnicity', add_race_ethnicity) \
+        .rename({'Payroll Number': 'payroll_number',
+                 'First Name': 'first_name',
+                 'Last Name': 'last_name',
+                 'Department': 'department',
+                 'Position': 'position',
+                 'Job': 'job',
+                 'Latest Start Date': 'latest_start_date',
+                 'Job Seniority Date': 'job_seniority_date',
+                 'Total Salary': 'total_salary',
+                 'Gender': 'gender',
+                 'Assignment Status': 'assignment_status',
+                 'Assignment Category': 'assignment_category'}) \
+        .select('{latest_start_date} == {job_seniority_date}') \
+        .addfield('race_ethnicity', add_race_ethnicity) \
         .cutout(*RACES)
     print(etl.tocsv(table))
 
